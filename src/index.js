8483391d6bacc4fba1370099126fcd44876f5f4e
Misha <73774460+FedchenkoM@users.noreply.github.com>
2021-08-04 11:55:59 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return isNotEmpty(array) ? Math.min(...array) : 0
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return isNotEmpty(array) ? Math.max(...array) : 0
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (isNotEmpty(array)) {
+        return array.reduce((acc, cur) => acc += cur, 0) / array.length
+    }
+    return 0
 }
+
+function isNotEmpty(arr) {
+    return (arr && arr.length > 0) ? true : false
+}
+
